f88b3dc59082a39a3a1c87ee144240e74987a23c
Vladimir Fibe <motiw@icloud.com>
2023-07-19 15:45:59 +0600

    Initial Commit

---
+++ RealTimeChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimeChat
+//
+//  Created by Vladimir Fibe on 19.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
